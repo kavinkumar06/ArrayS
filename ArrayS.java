acb0a7f62c9a5c899e2380ddf1d6d253dd4896e5
kavinkumar06 <kavinm09@gmail.com>
2017-07-31 17:10:14 +0530

    Create ArrayS.java

---
+++ ArrayS.java
@@ -0,0 +1,26 @@
+# ArrayS
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class DecimalMac {
+
+	public static void main(String[] args) {
+
+	Scanner sr=new Scanner(System.in);
+	System.out.println("Enter the size of array");
+	double size=sr.nextDouble();
+	   
+		   double iArr[] =new double[(int) size];
+		   System.out.println("enter the element");
+		   for(int i=0;i<iArr.length;i++){
+			   iArr[i]=sr.nextInt();
+		   }
+
+
+	   
+	   Arrays.sort(iArr);
+
+	   for(int j=iArr.length-1;j>=0;j--){
+		   System.out.print(iArr[j]+" ");
+	   }
+	   }
